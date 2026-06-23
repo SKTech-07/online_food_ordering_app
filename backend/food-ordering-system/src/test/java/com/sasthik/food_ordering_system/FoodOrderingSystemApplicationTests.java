34fc1f75a9c4e22c7f14385c22287c6c1d15bc7b
SKTech-07 <sasthickkrishanmoorthy@gmail.com>
2026-06-23 22:27:53 +0530

    feat: setup Spring Boot backend project

---
+++ backend/food-ordering-system/src/test/java/com/sasthik/food_ordering_system/FoodOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasthik.food_ordering_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
